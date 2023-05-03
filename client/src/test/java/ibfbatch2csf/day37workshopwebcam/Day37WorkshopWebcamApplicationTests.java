b33f5945f8081790027a3fe1d2c0cad6d809c56c
Shiyi <yeoshiyi22@gmail.com>
2023-05-03 14:42:40 +0800

    first

---
+++ client/src/test/java/ibfbatch2csf/day37workshopwebcam/Day37WorkshopWebcamApplicationTests.java
@@ -0,0 +1,13 @@
+package ibfbatch2csf.day37workshopwebcam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day37WorkshopWebcamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ server/src/main/java/ibfbatch2csf/day37workshopwebcam/Day37WorkshopWebcamApplication.java
@@ -0,0 +1,13 @@
+package ibfbatch2csf.day37workshopwebcam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day37WorkshopWebcamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day37WorkshopWebcamApplication.class, args);
+	}
+
+}
